70009c99fb471a1408387e6d4abf9d2cebc35d8f
huihui <18040551470@163.com>
2018-02-24 18:04:21 +0800

    third commit
    1.新增了短信验证登录
    2.新增了可以加载网络地址的自定义图片MyImageView
    3.新增了给EditText添加清除内容的功能

---
+++ app/src/main/java/xidaiw/mine/activity/LoginBySmsActivity.java
@@ -0,0 +1,13 @@
+package xidaiw.mine.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginBySmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_by_sms);
+    }
+}
---
+++ app/src/main/java/xidaiw/widget/MyImageView.java
@@ -0,0 +1,10 @@
+package xidaiw.widget;
+
+import android.widget.ImageView;
+
+/**
+ * Created by admin on 2018/2/24.
+ */
+
+public class MyImageView extends ImageView {
+}
